8998bd4bbe956d101b40e7c6a859d6ccf7eeb34a
Kelsey Cahill <kelscahill@gmail.com>
2020-12-16 21:16:14 -0500

    Restructured the app and added animations.

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,12 @@
             <Logo />
           </header>
           <div class="c-panel__content">
-            <div class="c-panel__content-body u-spacing">
-              <Branding />
-              <div class="o-heading--l">202<span class="u-xout">0</span> should be left on the cutting room floor. Still, there are a few bright bits worth saving. Some quick <div class="u-underline"><span>revisions</span></div> could make a world of difference for the year ahead.</div>
+            <div class="c-panel__content-body u-spacing animate__animated animate__fadeIn animate__slower">
+              <div class="o-heading--m">202<span class="u-xout">0</span> should be left on the cutting room floor. Still, there are a few bright bits worth saving. Some quick <div class="u-underline"><span>revisions</span></div> could make a world of difference for the year ahead.</div>
             </div>
             <footer class="c-panel__content-footer">
-              <button v-on:click="isHidden = false" class="o-button--primary animate__animated animate__pulse animate__delay-5s">Start revising!</button>
+              <button v-on:click="isHidden = false" class="o-button--primary animate__animated animate__fade-in-up animate__delay-1s">Start revising!</button>
+              <Branding />
             </footer>
           </div>
         </article>
@@ -27,35 +27,40 @@
           :class="{'is-revised':isRevised}"
         >
           <header class="c-panel__hero">
-            <div class="o-counter">{{ resolutionIndex + 1 }}/{{ quiz.resolutions.length }}</div>
-            <h2 class="o-heading--xl">{{ quiz.resolutions[resolutionIndex].statement }} <div class="o-habit is-active" v-if="!isRevised" v-html="quiz.resolutions[resolutionIndex].habit"></div><div class="o-habit is-revised" v-if="isRevised" v-html="results.button[resolutionIndex]"></div>.</h2>
+            <div class="o-progress">
+              <span>{{ resolutionIndex + 1 }}/{{ quiz.resolutions.length }}</span>
+              <progress :value="(resolutionIndex + 1) * 20" max="100"></progress>
+            </div>
+            <div class="c-panel__hero-heading">
+              <h2 class="o-heading--xl">
+                <span class="o-statement" v-html="quiz.resolutions[resolutionIndex].statement"></span>{{' '}}
+                <span class="o-habit animate__animated" v-if="!isRevised" v-html="quiz.resolutions[resolutionIndex].habit"></span>
+                <span class="o-habit animate__animated is-revised" v-if="isRevised" v-html="results.button[resolutionIndex]"></span>.
+              </h2>
+            </div>
           </header>
           <div class="c-panel__content">
             <div class="c-panel__content-body">
-              <div class="c-panel__options" v-if="!isRevised">
+              <div class="c-panel__options u-spacing--half" v-if="!isRevised">
                 <button
-                  class="o-button--tertiary"
+                  class="o-button--secondary animate__animated animate__fade-in-up"
                   v-bind:key="revision.id"
                   v-for="(revision, index) in quiz.resolutions[resolutionIndex].revisions"
-                  v-on:click="selected(index)"
-                  :class="{'is-active':isSelected == index + 1}"
+                  v-on:click="revise(index, resolutionIndex)"
+                  :id="'o-revision--' + resolutionIndex + '-' + index"
                   :data-button="revision.button"
                   :data-value="revision.value"
-                  :data-description="revision.description"
+                  :data-gif="revision.gif"
                   v-html="revision.button"
                 ></button>
               </div>
-              <div class="c-panel__description u-spacing animate__animated animate__fadeIn animate__delay-1s" v-if="isRevised" v-html="results.description[resolutionIndex]"></div>
+              <div class="c-panel__gif u-spacing animate__animated animate__fadeIn" v-if="isRevised">
+                <img :src="results.gif[resolutionIndex]" alt="Gif" />
+              </div>
             </div>
             <footer class="c-panel__content-footer">
               <button
-                class="o-button--secondary"
-                v-if="!isRevised"
-                v-on:click="revise(resolutionIndex)"
-                :disabled="isSelected <= 0"
-              >{{ isSelected ? 'Revise it!' : 'Choose a Revision above' }}</button>
-              <button
-                class="o-button--secondary"
+                class="o-button--secondary animate__animated animate__fade-in-up"
                 v-if="isRevised"
                 v-on:click="next"
               >{{ quiz.resolutions[resolutionIndex].button_text }}</button>
@@ -65,28 +70,44 @@
         </article>
 
         <article
-          v-if="resolutionIndex >= quiz.resolutions.length"
-          class="c-panel c-panel--results"
+          v-if="resolutionIndex >= quiz.resolutions.length && resultIsHidden"
+          class="c-panel c-panel--complete"
         >
           <header class="c-panel__hero">
-            <div class="c-results-card">
-              <img src="./assets/logo.svg" alt="2020 Revised" class="c-results-card__logo">
-              <div class="c-results-card__body o-heading--m">Watch out world! This <div class="u-strike" v-html="quiz.resolutions[0].habit"></div> <div class="u-underline" v-html="results.value[0]"></div> superstar is ready to <div class="u-strike" v-html="quiz.resolutions[1].habit"></div> <div class="u-underline" v-html="results.value[1]"></div> and take 2021 by <div class="u-strike" v-html="quiz.resolutions[2].habit"></div> <div class="u-underline" v-html="results.value[2]"></div> storm. I’ll start by <div class="u-strike" v-html="quiz.resolutions[3].habit"></div> <div class="u-underline" v-html="results.value[3]"></div> and promise to always have <div class="u-strike" v-html="quiz.resolutions[4].habit"></div> <div class="u-underline" v-html="results.value[4]"></div> at the ready.  Here’s to a happier new year!</div>
-              <div class="c-results-card__website">2020Revised.com</div>
-            </div>
+            <h2 class="o-heading--xl">You did it!</h2>
+            <img src="./assets/swivel.svg" alt="Swivel" />
           </header>
           <div class="c-panel__content">
             <div class="c-panel__content-body">
-              <h3 class="o-heading--l">All of us at Ashton Design hope your new year revisions bring you a happy and healthier 2021.</h3>
+              <h3 class="o-heading--m">All of us at Ashton Design hope your new year revisions bring you a happy and healthier 2021.</h3>
             </div>
             <footer class="c-panel__content-footer">
-              <div class="c-social-share" v-if="!socialIsHidden">
+              <button class="o-button--tertiary" v-on:click="resultIsHidden = !resultIsHidden">Final approval</button>
+              <Branding />
+            </footer>
+          </div>
+        </article>
+
+        <article
+          v-if="resolutionIndex >= quiz.resolutions.length && !resultIsHidden"
+          class="c-panel c-panel--results"
+        >
+          <header class="c-panel__hero">
+            <div id="card-square">
+              <ResultsCard :results="results" />
+            </div>
+            <ResultsCard :results="results" />
+          </header>
+          <div class="c-panel__content">
+            <footer class="c-panel__content-footer">
+              <div class="c-social-share" v-if="!shareIsHidden">
                 <a href="">Facebook</a>
                 <a href="">Twitter</a>
                 <a href="">Email</a>
                 <a href="">SMS</a>
+                <a id="download-square" download="2020-revised.jpg">Download Image</a>
               </div>
-              <button class="o-button" v-on:click="socialIsHidden = !socialIsHidden">Share Your Revisions!</button>
+              <button class="o-button" v-on:click="screenshot(); shareIsHidden = !shareIsHidden">Share Your Revisions!</button>
               <Branding />
             </footer>
           </div>
@@ -109,25 +130,25 @@ var quiz = {
         {
           id: 1,
           button: "<span>reconnecting</span><span>with family</span>",
-          description: "<h4><span>reconnecting</span><span>with family</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>reconnecting</span><span>with family</span>"
         },
         {
           id: 2,
           button: "<span>exercising</span>",
-          description: "<h4><span>exercising</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>exercising</span>"
         },
         {
           id: 3,
           button: "<span>outside</span>",
-          description: "<h4><span>outside</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>outside</span>"
         },
         {
           id: 4,
           button: "<span>drinking</span><span>better wine</span>",
-          description: "<h4><span>drinking</span><span>better wine</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>drinking</span><span>better wine</span>"
         }
       ]
@@ -141,25 +162,25 @@ var quiz = {
         {
           id: 1,
           button: "<span>read a</span><span>book</span>",
-          description: "<h4><span>read a</span><span>book</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>read a</span><span>book</span>"
         },
         {
           id: 2,
           button: "<span>drink a cup</span><span>of tea</span>",
-          description: "<h4><span>drink a cup</span><span>of tea</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>drink a cup</span><span>of tea</span>"
         },
         {
           id: 3,
           button: "<span>turn off</span><span>my phone</span>",
-          description: "<h4><span>turn off</span><span>my phone</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>turn off</span><span>my phone</span>"
         },
         {
           id: 4,
           button: "<span>practice my</span><span>dance moves</span>",
-          description: "<h4><span>practice my</span><span>dance moves</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>practice my</span><span>dance moves</span>"
         }
       ]
@@ -173,25 +194,25 @@ var quiz = {
         {
           id: 1,
           button: "<span>relaxed</span>",
-          description: "<h4><span>relaxed</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>relaxed</span>"
         },
         {
           id: 2,
           button: "<span>lively</span>",
-          description: "<h4><span>lively</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>lively</span>"
         },
         {
           id: 3,
           button: "<span>intelligent</span>",
-          description: "<h4><span>intelligent</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>intelligent</span>"
         },
         {
           id: 4,
           button: "<span>healthy</span>",
-          description: "<h4><span>healthy</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>healthy</span>"
         }
       ]
@@ -205,25 +226,25 @@ var quiz = {
         {
           id: 1,
           button: "<span>making my bed</span>",
-          description: "<h4><span>making my bed</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>making my bed</span>"
         },
         {
           id: 2,
           button: "<span>meditating</span><span>more</span>",
-          description: "<h4><span>meditating</span><span>more</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>meditating</span><span>more</span>"
         },
         {
           id: 3,
           button: "<span>showering</span><span>regularly</span>",
-          description: "<h4><span>showering</span><span>regularly</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>showering</span><span>regularly</span>"
         },
         {
           id: 4,
           button: "<span>reading my</span><span>horoscope</span>",
-          description: "<h4><span>reading my</span><span>horoscope</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>reading my</span><span>horoscope</span>"
         }
       ]
@@ -237,25 +258,25 @@ var quiz = {
         {
           id: 1,
           button: "<span>self-love</span>",
-          description: "<h4><span>self-love</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>self-love</span>"
         },
         {
           id: 2,
           button: "<span>funny memes</span>",
-          description: "<h4>funny memes</h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>funny memes</span>"
         },
         {
           id: 3,
           button: "<span>empathy</span>",
-          description: "<h4><span>empathy</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>empathy</span>"
         },
         {
           id: 4,
           button: "<span>phone calls</span><span>to friends</span>",
-          description: "<h4><span>phone calls</span><span>to friends</span></h4><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>",
+          gif: "https://media.giphy.com/media/slOhiKAVFgwr6/source.gif",
           value: "<span>phone calls</span><span>to friends</span>"
         }
       ]
@@ -265,25 +286,28 @@ var quiz = {
 
 import Logo from './components/Logo.vue'
 import Branding from './components/Branding.vue'
+import ResultsCard from './components/ResultsCard.vue'
+import html2canvas from 'html2canvas'
 
 export default {
   name: "App",
   components: {
     Logo,
-    Branding
+    Branding,
+    ResultsCard,
   },
   data() {
     return {
       quiz: quiz,
       resolutionIndex: 0,
-      socialIsHidden: true,
+      shareIsHidden: true,
+      resultIsHidden: true,
       isHidden: true,
       isRevised: false,
-      isSelected: 0,
       results: {
         button: [],
         value: [],
-        description: []
+        gif: []
       }
     }
   },
@@ -292,18 +316,28 @@ export default {
       if (this.resolutionIndex < this.quiz.resolutions.length) {
         this.resolutionIndex++;
       }
-      this.isSelected = 0;
       this.isRevised = false;
     },
-    selected(index) {
-      this.isSelected = index + 1;
-    },
-    revise(index) {
+    revise(index, resolutionIndex) {
+      var id = resolutionIndex + '-' + index;
       this.isRevised = true;
-      this.results.button[index] = document.querySelector('.o-button--tertiary.is-active').dataset.button;
-      this.results.value[index] = document.querySelector('.o-button--tertiary.is-active').dataset.value;
-      this.results.description[index] = document.querySelector('.o-button--tertiary.is-active').dataset.description;
-    }
+      this.results.button[resolutionIndex] = document.getElementById("o-revision--" + id).dataset.button;
+      this.results.value[resolutionIndex] = document.getElementById("o-revision--" + id).dataset.value;
+      this.results.gif[resolutionIndex] = document.getElementById("o-revision--" + id).dataset.gif;
+    },
+    screenshot() {
+      var cardSquare = document.getElementById("card-square");
+      html2canvas(cardSquare, {
+        width: 600,
+        height: 600
+      }).then(function(canvas) {
+        var image = canvas.toDataURL("image/jpg");
+        var downloadSquareButton = document.getElementById("download-square");
+        if (image && downloadSquareButton.href === "") {
+          downloadSquareButton.href = image;
+        }
+      });
+    },
   }
 }
 </script>
---
+++ src/components/ResultsCard.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="c-results-card">
+    <img src="../assets/logo.png" alt="2020 Revised" class="c-results-card__logo">
+    <div class="c-results-card__body o-heading--l">Watch out world! This <div class="u-underline" v-html="results.value[0]"></div> superstar is ready to <div class="u-underline" v-html="results.value[1]"></div> and take 2021 by <div class="u-underline" v-html="results.value[2]"></div> storm. I’ll start by <div class="u-underline" v-html="results.value[3]"></div> and promise to always have <div class="u-underline" v-html="results.value[4]"></div> at the ready.  Here’s to a happier new year!</div>
+    <div class="c-results-card__website">2020Revised.com</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ResultsCard",
+  props: ["results"]
+}
+</script>
